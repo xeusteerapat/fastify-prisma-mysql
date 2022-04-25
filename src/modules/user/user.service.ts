6040f756ebce71f2a0648313535d87f8b4a420cd
xeusteerapat <xeus085@gmail.com>
2022-04-25 12:21:52 +0700

    :sparkles: [Update] find users service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -17,3 +17,25 @@ export const createUser = async (input: CreateUserInputSchema) => {
 
   return user;
 };
+
+export const findUserByEmail = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  return user;
+};
+
+export const findUsers = async () => {
+  const users = await prisma.user.findMany({
+    select: {
+      email: true,
+      name: true,
+      id: true,
+    },
+  });
+
+  return users;
+};
