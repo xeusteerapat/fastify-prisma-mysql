f6ff4551b2bafdb73f2ca7a75eac3c349553166e
xeusteerapat <xeus085@gmail.com>
2022-04-25 10:27:20 +0700

    :sparkles: [Update] update user schemas

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
+import { buildJsonSchemas } from 'fastify-zod';
 
-const createUserSchema = z.object({
+const userCoreSchema = {
   email: z
     .string({
       required_error: 'Email is required',
@@ -8,10 +9,24 @@ const createUserSchema = z.object({
     })
     .email(),
   name: z.string(),
+};
+
+const createUserSchema = z.object({
+  ...userCoreSchema,
   password: z.string({
     required_error: 'Password is required',
     invalid_type_error: 'Password must be a string',
   }),
 });
 
+const createUserResponseSchema = z.object({
+  ...userCoreSchema,
+  id: z.number(),
+});
+
 export type CreateUserInputSchema = z.infer<typeof createUserSchema>;
+
+export const { schemas: userSchemas, $ref } = buildJsonSchemas({
+  createUserSchema,
+  createUserResponseSchema,
+});
