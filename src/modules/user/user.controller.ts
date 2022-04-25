b85d76ec35428d3876435c4830af9c9e178ee6e9
xeusteerapat <xeus085@gmail.com>
2022-04-25 10:26:16 +0700

    :sparkles: [Add] create user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,21 @@
+import { CreateUserInputSchema } from './user.schema';
+import { createUser } from './user.service';
 import { FastifyRequest, FastifyReply } from 'fastify';
 
 export async function registerUserHandler(
-  request: FastifyRequest,
+  request: FastifyRequest<{
+    Body: CreateUserInputSchema;
+  }>,
   reply: FastifyReply
-) {}
+) {
+  const { body } = request;
+
+  try {
+    const user = await createUser(body);
+
+    return reply.code(201).send(user);
+  } catch (error) {
+    console.log(error);
+    return reply.code(500).send(error);
+  }
+}
