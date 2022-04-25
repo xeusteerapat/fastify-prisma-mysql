60f23be41c974c333c1ef77a513d582725c41512
xeusteerapat <xeus085@gmail.com>
2022-04-25 12:16:47 +0700

    :sparkles: [Update] update login schema

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -24,9 +24,26 @@ const createUserResponseSchema = z.object({
   id: z.number(),
 });
 
+const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email must be a string',
+    })
+    .email(),
+  password: z.string(),
+});
+
+const loginResponseSchema = z.object({
+  accessToken: z.string(),
+});
+
 export type CreateUserInputSchema = z.infer<typeof createUserSchema>;
+export type LoginInputSchema = z.infer<typeof loginSchema>;
 
 export const { schemas: userSchemas, $ref } = buildJsonSchemas({
   createUserSchema,
   createUserResponseSchema,
+  loginSchema,
+  loginResponseSchema,
 });
